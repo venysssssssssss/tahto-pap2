db691fbdf197bfe2866ed369711b6a472f5e5abf
venysssssssssss <shablaur7@hotmail.com>
2024-08-05 11:38:56 -0300

    refactor: Improve data processing efficiency and error handling

--- app/monitor_falhas.py
+++ app/monitor_falhas.py
@@ -1,6 +1,7 @@
-import time
 import logging
+import time
 from threading import Thread
+
 import schedule
 from schedule_regular import schedule_regular_collections
 from selenium import webdriver
@@ -11,6 +12,7 @@ from send_telegram_msg import send_telegram_message
 # Configurar logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger()
+
 
 def collect_info(driver):
     try:
@@ -25,7 +27,10 @@ def collect_info(driver):
             status_xpath = f'{base_xpath}[{row}]/div[7]'
             item = driver.find_element(By.XPATH, item_xpath).text
             status = driver.find_element(By.XPATH, status_xpath).text
-            if item == 'ValidarVendasLiberadas' and status == 'Falha de sistema':
+            if (
+                item == 'ValidarVendasLiberadas'
+                and status == 'Falha de sistema'
+            ):
                 falha_detectada = True
             logger.info(f'Item: {item} - Status: {status}')
         return falha_detectada
@@ -33,25 +38,31 @@ def collect_info(driver):
         logger.error(f'Erro ao coletar informações: {e}')
         return False
 
+
 def run_scheduled_jobs():
     while True:
         schedule.run_pending()
         time.sleep(1)
 
+
 def monitor_falhas(driver, tme_xpath, tef_xpath, backlog_xpath):
-    logger.info("Iniciando monitoramento de falhas")
+    logger.info('Iniciando monitoramento de falhas')
     schedule_regular_collections(driver, tme_xpath, tef_xpath, backlog_xpath)
     scheduler_thread = Thread(target=run_scheduled_jobs)
     scheduler_thread.start()
-    logger.info("Thread de agendamento iniciada")
+    logger.info('Thread de agendamento iniciada')
 
     # Loop de monitoramento de falhas
     while True:
         falha_detectada = collect_info(driver)
         if falha_detectada:
-            send_telegram_message('Falha de sistema\n\nℹ️ Informação: falha ao importar pedidos')
+            send_telegram_message(
+                'Falha de sistema\n\nℹ️ Informação: falha ao importar pedidos'
+            )
             while falha_detectada:
                 time.sleep(60)
                 falha_detectada = collect_info(driver)
-            send_telegram_message('✅ Robô retomado para produção - MVP1 ✅\n\n⏰ Status: operando normalmente')
+            send_telegram_message(
+                '✅ Robô retomado para produção - MVP1 ✅\n\n⏰ Status: operando normalmente'
+            )
         time.sleep(60)
