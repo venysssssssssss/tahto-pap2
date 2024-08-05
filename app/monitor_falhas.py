1a514bbad0cb1b1134bf07c60c61b35901e46be2
venysssssssssss <shablaur7@hotmail.com>
2024-08-05 16:33:38 -0300

    refactor: Improve error handling and efficiency in monitor_falhas.py

--- app/monitor_falhas.py
+++ app/monitor_falhas.py
@@ -1,10 +1,8 @@
-import logging
 import time
+import logging
 from threading import Thread
-
 import schedule
 from schedule_regular import schedule_regular_collections
-from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from send_telegram_msg import send_telegram_message
@@ -12,7 +10,6 @@ from send_telegram_msg import send_telegram_message
 # Configurar logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger()
-
 
 def collect_info(driver):
     try:
@@ -40,31 +37,26 @@ def collect_info(driver):
         logger.error(f'Erro ao coletar informações: {e}')
         return False
 
-
 def run_scheduled_jobs():
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-
 def monitor_falhas(driver, tme_xpath, tef_xpath, backlog_xpath):
-    logger.info('Iniciando monitoramento de falhas')
+    logger.info("Iniciando monitoramento de falhas")
     schedule_regular_collections(driver, tme_xpath, tef_xpath, backlog_xpath)
     scheduler_thread = Thread(target=run_scheduled_jobs)
     scheduler_thread.start()
-    logger.info('Thread de agendamento iniciada')
+    logger.info("Thread de agendamento iniciada")
 
     # Loop de monitoramento de falhas
+    falha_reportada = False
     while True:
         falha_detectada = collect_info(driver)
-        if falha_detectada:
-            send_telegram_message(
-                '⚠️ MVP2 - Falha de sistema\n\nℹ️ Informação: falha ao importar pedidos'
-            )
-            while falha_detectada:
-                time.sleep(60)
-                falha_detectada = collect_info(driver)
-            send_telegram_message(
-                '✅ Robô retomado para produção - MVP2 ✅\n\n⏰ Status: operando normalmente'
-            )
+        if falha_detectada and not falha_reportada:
+            send_telegram_message('🤖 *MVP2 - Falha de sistema* ❌\n\nℹ️ *Informação*: falha ao importar pedidos')
+            falha_reportada = True
+        elif not falha_detectada and falha_reportada:
+            send_telegram_message('🤖 *MVP2 - Em produção* ✅\n\n⏰ *Status*: operando normalmente')
+            falha_reportada = False
         time.sleep(60)
--- app/send_telegram_msg.py
+++ app/send_telegram_msg.py
@@ -1,76 +1,61 @@
 import datetime
-import logging
 import os
-import traceback
-
 import requests
-from browser import BrowserManager
+import logging
+import traceback
 from data_processing import DataProcessor
+from browser import BrowserManager
 
-logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s'
-)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 browser_manager = BrowserManager('data')
 download_path = browser_manager.clean_download_directory('data')
 
-
-def send_telegram_message(message):
-    telegram_token = os.getenv(
-        'TELEGRAM_TOKEN', '7226155746:AAEBPeOtzJrD_KQyeZinNBjh5HMmvHTBZLs'
-    )
+def send_telegram_message(message, max_retries=3):
+    telegram_token = os.getenv('TELEGRAM_TOKEN', '7226155746:AAEBPeOtzJrD_KQyeZinNBjh5HMmvHTBZLs')
     chat_id = os.getenv('TELEGRAM_CHAT_ID', '-1002165188451')
     url = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
     payload = {'chat_id': chat_id, 'text': message, 'parse_mode': 'Markdown'}
 
-    try:
-        response = requests.post(url, data=payload)
-        response.raise_for_status()
-        logging.info('Mensagem enviada com sucesso!')
-    except requests.HTTPError as e:
-        logging.error(
-            f'HTTP error occurred: {e.response.status_code} - {e.response.text}'
-        )
-        traceback.print_exc()
-    except requests.RequestException as e:
-        logging.exception('Erro ao enviar mensagem: %s', e)
-
+    for attempt in range(1, max_retries + 1):
+        try:
+            response = requests.post(url, data=payload)
+            response.raise_for_status()
+            logging.info("Mensagem enviada com sucesso!")
+            return
+        except requests.HTTPError as e:
+            logging.error(f"HTTP error occurred: {e.response.status_code} - {e.response.text}")
+            traceback.print_exc()
+        except requests.RequestException as e:
+            logging.exception("Erro ao enviar mensagem: %s", e)
+        logging.info(f"Tentativa {attempt} de {max_retries} falhou, tentando novamente...")
+    logging.error("Falha ao enviar a mensagem após várias tentativas.")
 
 def send_informational_message(driver, tme_xpath, tef_xpath, backlog_xpath):
     try:
-        data_processor = DataProcessor(
-            os.path.join(download_path, 'relatorio.csv')
-        )
+        data_processor = DataProcessor(os.path.join(download_path, 'relatorio.csv'))
         metrics = data_processor.analyze_data()
         print(metrics)
 
         if metrics == 'no_data':
             message = (
                 f'🤖 *Automação PAP - MVP2*\n{datetime.date.today().strftime("%d/%m/%Y")}\n\n'
-                '⚠️Robô Ocioso (Sem Dados)'
+                'Robô Ocioso (Sem Dados)'
             )
             send_telegram_message(message)
-            logging.info('Mensagem de ociosidade enviada com sucesso!')
+            logging.info("Mensagem de ociosidade enviada com sucesso!")
             return
 
         count_success = metrics.get('count_success', 0)
         count_business_error = metrics.get('count_business_error', 0)
         count_system_failure = metrics.get('count_system_failure', 0)
-        total_processos = (
-            count_success + count_business_error + count_system_failure
-        )
+        total_processos = count_success + count_business_error + count_system_failure
 
         if total_processos > 0:
             percent_success = (count_success / total_processos) * 100
-            percent_business_error = (
-                count_business_error / total_processos
-            ) * 100
-            percent_system_failure = (
-                count_system_failure / total_processos
-            ) * 100
+            percent_business_error = (count_business_error / total_processos) * 100
+            percent_system_failure = (count_system_failure / total_processos) * 100
         else:
-            percent_success = (
-                percent_business_error
-            ) = percent_system_failure = 0
+            percent_success = percent_business_error = percent_system_failure = 0
 
         message = (
             f'🤖 *Automação PAP - MVP2*\n{datetime.date.today().strftime("%d/%m/%Y")}\n\n'
@@ -86,10 +71,8 @@ def send_informational_message(driver, tme_xpath, tef_xpath, backlog_xpath):
             f'🔰 Informacional desenv. - Projetos Tahto Aut/IA 🔰'
         )
         send_telegram_message(message)
-        logging.info('Mensagem processada e enviada com sucesso!')
+        logging.info("Mensagem processada e enviada com sucesso!")
     except KeyError as e:
-        logging.exception('Chave ausente nos dados: %s', e)
+        logging.exception("Chave ausente nos dados: %s", e)
     except Exception as e:
-        logging.exception(
-            'Erro inesperado ao enviar mensagem informativa: %s', e
-        )
+        logging.exception("Erro inesperado ao enviar mensagem informativa: %s", e)
