de15dfc001597607c030a66b0558cf636b1df89f
venysssssssssss <shablaur7@hotmail.com>
2024-08-05 11:38:51 -0300

    refactor: Improve data processing efficiency and error handling

--- app/main.py
+++ app/main.py
@@ -1,20 +1,22 @@
+import logging
 import os
 import time
-import logging
+
 from action_manager import ActionManager
 from authentication import Authenticator
 from browser import BrowserManager
 from data_processing import DataProcessor
-from send_telegram_msg import send_informational_message
 from monitor_falhas import monitor_falhas
 from selenium.webdriver.common.by import By
+from send_telegram_msg import send_informational_message
 
 # Configurar logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger()
 
+
 def main():
-    logger.info("Iniciando o script principal")
+    logger.info('Iniciando o script principal')
     browser_manager = BrowserManager('data')
     download_path = browser_manager.clean_download_directory('data')
     try:
@@ -22,7 +24,7 @@ def main():
         actions = ActionManager(browser_manager.driver)
         auth = Authenticator(browser_manager.driver)
         auth.authenticate()
-        logger.info("Autenticação concluída")
+        logger.info('Autenticação concluída')
 
         # Coleta de KPIs adicionais
         actions.click_element('//*[@id="var-exibir"]')
@@ -30,18 +32,24 @@ def main():
         tme_element = actions.find_element(
             '//*[@id=":ri:"]/div/div/div/div[1]/div/div/span'
         )
-        tme_xpath = tme_element.text if tme_element.text != 'No data' else '00:00:00'
+        tme_xpath = (
+            tme_element.text if tme_element.text != 'No data' else '00:00:00'
+        )
         tef_element = actions.find_element(
             '//*[@id=":rj:"]/div/div/div/div/div/div/span'
         )
-        tef_xpath = tef_element.text if tef_element.text != 'No data' else '00:00:00'
+        tef_xpath = (
+            tef_element.text if tef_element.text != 'No data' else '00:00:00'
+        )
         backlog_xpath = actions.find_element(
             '//*[@id=":re:"]/div/div/div/div/div/div/span'
         ).text
-        logger.info(f"KPIs coletados: TME={tme_xpath}, TEF={tef_xpath}, Backlog={backlog_xpath}")
+        logger.info(
+            f'KPIs coletados: TME={tme_xpath}, TEF={tef_xpath}, Backlog={backlog_xpath}'
+        )
 
         browser_manager.scroll_to_table()
-        logger.info("Scroll até a tabela concluído")
+        logger.info('Scroll até a tabela concluído')
 
         actions.move_to_and_interact('//*[@id=":rl:"]', 'i')
         actions.click_element(
@@ -56,24 +64,26 @@ def main():
         actions.click_element(
             '//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div[2]/button'
         )
-        logger.info("Ações para download concluídas")
+        logger.info('Ações para download concluídas')
 
         # Aguarda o download ser concluído
         browser_manager.wait_for_download_complete(download_path)
         browser_manager.rename_latest_file(download_path, 'relatorio.csv')
-        logger.info("Download concluído e arquivo renomeado")
+        logger.info('Download concluído e arquivo renomeado')
 
         actions.click_element(
             '//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[1]/div[1]/button'
         )
 
-        logger.info("Monitoramento de falhas iniciado")
-        monitor_falhas(browser_manager.driver, tme_xpath, tef_xpath, backlog_xpath)
-        
+        logger.info('Monitoramento de falhas iniciado')
+        monitor_falhas(
+            browser_manager.driver, tme_xpath, tef_xpath, backlog_xpath
+        )
 
     finally:
         browser_manager.quit_browser()
-        logger.info("Finalizando o script principal")
+        logger.info('Finalizando o script principal')
+
 
 if __name__ == '__main__':
     main()
